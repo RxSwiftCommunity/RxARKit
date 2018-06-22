92e30588da2f291fcc2bfaf2b76ddde1745d5a30
Maxim Volgin <maxim.volgin@me.com>
2018-06-22 20:13:04 +0200

    initial

---
+++ RxARKit/RxARKit.h
@@ -0,0 +1,19 @@
+//
+//  RxARKit.h
+//  RxARKit
+//
+//  Created by Maxim Volgin on 22/06/2018.
+//  Copyright © 2018 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxARKit.
+FOUNDATION_EXPORT double RxARKitVersionNumber;
+
+//! Project version string for RxARKit.
+FOUNDATION_EXPORT const unsigned char RxARKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxARKit/PublicHeader.h>
+
+
